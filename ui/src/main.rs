50e357096435945c0092e4520bccd9644e945b9c
Ayush <ayushmodi2003@gmail.com>
2023-07-18 02:18:14 +0530

    Stream block nums

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,6 +1,6 @@
 use std::error;
 use gloo_net::websocket::{Message, futures::WebSocket};
-use futures::StreamExt;
+use futures::{StreamExt, SinkExt};
 use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
 
@@ -84,6 +84,60 @@ fn epoch_num() -> Html {
     }
 }
 
+// DOES NOT WORK ATM - POTENTIALLY DUE TO ISSUES WITH GETH
+// UNABLE TO CONNECT TO THE ENDPOINT
+// NETSTAT BLANK FOR THE PORT
+#[function_component(BlockNum)]
+fn block_num() -> Html {
+	let block = use_state(|| None);
+
+    {
+        let block = block.clone();
+        use_effect(move || {
+            if block.is_none() {
+                let ws = WebSocket::open("ws://127.0.0.1:8546").unwrap();
+                let (mut write, mut read) = ws.split();
+				
+				spawn_local(async move {
+					let data = r#"
+						{
+							"id": 1, 
+							"jsonrpc": "2.0", 
+							"method": "eth_subscribe", 
+							"params": ["newHeads"]
+						}"#;
+					write.send(Message::Text(String::from(data))).await.unwrap();
+				});
+
+                spawn_local(async move {
+                    while let Some(msg) = read.next().await {
+                        let result = msg.unwrap();
+                        match result {
+                            Message::Text(text) => {
+                                block.set(Some(text));
+                            },
+                            _ => {},
+                        };
+                    }
+                });
+            }
+            || {}
+        });
+    }
+
+    html! {
+        <div id="block">
+            { 
+              match block.as_ref() {
+                None => html! {"heyy"},
+                Some(data) => html! { format!("Block: {}", data) },
+              }
+            }
+        </div>
+    }
+}
+
+
 
 #[function_component(App)]
 fn app() -> Html {
@@ -91,6 +145,7 @@ fn app() -> Html {
 		<>
 			<SlotNum /> 
 			<EpochNum />
+			<BlockNum />
 		</>
 	}
 }
